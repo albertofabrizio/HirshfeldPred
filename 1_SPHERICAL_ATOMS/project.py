38cc9312e7250be56ff5d41247f7c18e24c50666
Alberto Fabrizio <alberto.fabrizio@mail.com>
2021-07-12 17:25:49 +0200

    added deco (buggy)

--- 1_SPHERICAL_ATOMS/project.py
+++ 1_SPHERICAL_ATOMS/project.py
@@ -1,90 +1,150 @@
 import numpy as np
-import ase.io as aio
-from pyscf import gto, dft
-from pyscf.dft import numint
+from pyscf import gto
 import argparse
-import json
 
-parser = argparse.ArgumentParser(description='Process the input.')
-parser.add_argument('--mol', type=str, nargs='+', dest='filename',
-                    help='Molecular structure in xyz format')
-parser.add_argument('--basis', type=str, nargs='+', dest='basis',
-                    help='Basis set for DFT computation')
-parser.add_argument('--auxbasis', type=str, nargs='+', dest='auxbasis',
-                    help='Basis set for decomposition')
-parser.add_argument('--func', type=str, nargs='+', dest='xc_func',
-                    help='XC functional for computation')
+########################## Parsing user defined input ##########################
 
+parser = argparse.ArgumentParser(description="This programs projects the spherical averaged density matrix @ HF level onto a given basis set.")
+
+parser.add_argument('--atom', type=str, required=True, dest='atm', help="The periodic table symbol for the atom.")
+parser.add_argument('--dm', type=str, required=True, dest='dmfile', help="The path for the density matrix.")
+parser.add_argument('--basis', type=str, required=True, dest='base', help="The name of the basis set used for the DM computation.")
+parser.add_argument('--auxbasis', type=str, required=True, dest='auxbase', help="The name of the basis set for the projection.")
+parser.add_argument('--isJ', type=bool, dest='isJ', help="Whether or not using the Coulomb metric for projection.", default = True)
+parser.add_argument('--isfile', type=bool, dest='isfile', help="Whether or not the auxbasis is the name of an external file to read [default: False].", default = False)
 
 args = parser.parse_args()
 
-
-def read_xyz(filename):
-    return aio.read(filename, format='xyz')
-
-
-def convert_ASE_PySCF(molecules, basis, auxbasis):
-    # Take an ASE atom and return  PySCF mol and auxmol objects.
-
-    sym = molecules.get_chemical_symbols()
-    pos = molecules.positions
-    tmp_mol = []
-    for s, (x, y, z) in zip(sym, pos):
-        tmp_mol.append([s, (x, y, z)])
-
-    pyscf_mol = gto.M(atom=tmp_mol, basis=basis, charge=1)
-
-    with open(auxbasis, 'r') as f:
-        auxbasis = eval(f.read())
-
-    pyscf_auxmol = gto.M(atom=tmp_mol, basis=auxbasis, charge=1)
-
-    return pyscf_mol, pyscf_auxmol
-
+########################## Helper Functions ##########################
 
 def get_integrals(mol, auxmol):
+    """ Compute the overlap, the two-center and the three-center Coulomb integrals of the auxiliary basis functions.
+    
+    Returns:
+        The S, J2, and J3 integrals, 2D/3D ndarray
 
+    """
+
+    # Compute overlap
     S = auxmol.intor('int1e_ovlp_sph')
+
+    # Create a fake mol object concatenating mol and auxmol
     pmol = mol + auxmol
 
+    # Compute Coulomb integrals
     eri2c = auxmol.intor('int2c2e_sph')
-    eri3c = pmol.intor('int3c2e_sph', shls_slice=(
-        0, mol.nbas, 0, mol.nbas, mol.nbas, mol.nbas+auxmol.nbas))
+    eri3c = pmol.intor('int3c2e_sph', shls_slice=(0, mol.nbas, 0, mol.nbas, mol.nbas, mol.nbas+auxmol.nbas))
     eri3c = eri3c.reshape(mol.nao_nr(), mol.nao_nr(), -1)
 
     return S, eri2c, eri3c
 
 
-def get_coeff(dm, eri2c, eri3c):
+def get_coeff_J(dm, eri2c, eri3c):
+    """ Compute the expansion coefficients using the J-metric.
+    
+    Returns:
+        The coefficients, 1D ndarray
+
+    """
+
     rho = np.einsum('ijp,ij->p', eri3c, dm)
     c = np.linalg.solve(eri2c, rho)
     return c
 
+def overlap(a, b):
+
+
+    return 0
+
+def number_of_electrons(rho, mol):
+    """ Compute the number of electrons given coefficients for one atom.
+    
+    Returns:
+        The nuymber of electron, float
+
+    """
+
+    # Get information about the basis
+    bas_info = np.array(mol._bas)
+    basis = mol._basis[args.atm]
+
+    # Identify spherical GTOs
+    spherical_info = bas_info[bas_info[:,1] == 0]
+
+    # Loop over all the contracted spherical GTOs    
+    nel = 0
+    for n in range(spherical_info.shape[0]):
+        primitives = np.array(basis[n][1:])
+
+        # Get exponents and coefficients for this cGTO
+        es = primitives[:,0]
+        cs = primitives[:,1:]
+
+        cs = np.einsum('pi, p -> pi', cs, pow(2*es/np.pi, 0.75))
+
+        # Compute cGTO self-overlap and normalize
+        ss = 0
+        for ia in range(len(cs)):
+            for ib in range(len(cs)):
+
+                esa = es[ia]
+                esb = es[ib]
+
+                ss += cs[ia] * cs[ib] * pow(np.pi/(esa + esb),1.5)                
+
+        fact = 1/np.sqrt(ss)
+        cs = np.einsum('pi, p -> pi', cs, fact)
+
+        # Compute integral
+        integral = pow(np.pi/es, 1.5)
+        integral = np.dot(integral,cs)
+
+        # Compute number of electrons
+        stop = n + integral.shape[0]
+        nel += np.dot(rho[n:stop],integral)
+    
+    return nel
+
+########################## Main ##########################
 
 def main():
-    print('Reading molecule')
-    compounds = aio.read(args.filename[0]+".xyz", format='xyz')
-    mol, auxmol = convert_ASE_PySCF(compounds, args.basis[0], args.auxbasis[0])
-    print('Performing DFT computation')
-    dm = np.load(args.filename[0]+"_dm.npy")
-    print('Computing integrals')
-    S, eri2c, eri3c = get_integrals(mol, auxmol)
-    print('Computing coefficients')
-    c = get_coeff(dm, eri2c, eri3c)
-    print(c.shape)
+
+    # Read in inputs
+    atm = args.atm.upper()
+    print("Projecting DM of atom.", atm)    
+
+    # Define spin in function of element
+
+    spin_dict = {"H":1, "HE":0,
+    "LI":1, "BE":0, "B":1, "C":2, "N":3, "O":2, "F":1, "NE":0,
+    "NA":1, "MG":0, "AL":1, "SI":2, "P":3,"S":2,"CL":1, "AR":0,
+    "BR":1}
     
-    g = dft.gen_grid.Grids(mol)
-    g.level = 3
-    g.build()
-    coords = g.coords
-    grid_weights = g.weights
+    # Instance pyscf mol object
+    mol = gto.M(atom=atm+''' 0 0 0''', basis=args.base, spin=spin_dict[atm])
+    auxmol = gto.M(atom=atm+''' 0 0 0''', basis=args.auxbase, spin=spin_dict[atm])
 
-    ao_sph = numint.eval_ao(auxmol, coords)
+    # Loading the density matrix
+    dm = np.load(args.dmfile)
 
-    print(np.dot(np.dot(ao_sph, c),grid_weights))
+    # Compute basis set integrals
+    print('Computing integrals.')
+    S, eri2c, eri3c = get_integrals(mol, auxmol)
+
+    # Compute coefficients
+    print('Computing coefficients.')
+    if args.isJ:
+        c = get_coeff_J(dm, eri2c, eri3c)
+    else:
+        c = 0
+        
+    # Check the number of electrons after density-fitting.
+    print('Checking number of electrons.')
+    n = number_of_electrons(c, auxmol)
+    print(n)
 
     print('Saving coefficients')
-    np.save('coeff_'+args.filename[0], c)
+    np.save('coeff_'+args.atm, c)
 
 
 if __name__ == "__main__":
